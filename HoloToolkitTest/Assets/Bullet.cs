c90ffe3c2306504fbee25bda97fa0ca6c0afb07a
Jonny <jonnymarx98@gmail.com>
2017-12-07 13:33:58 +0000

    game is now played on the real world not testing plane

---
+++ HoloToolkitTest/Assets/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
